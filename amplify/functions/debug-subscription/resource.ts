c91e230ed05179cdf668788bb9e7e84ca0a1610d
Hillary Langat <hillangat@gmail.com>
2025-10-07 04:56:12 -0400

    Add resourceGroupName to debugSubscription function
    - Assign debugSubscription to data stack to prevent circular dependencies
    - Function accesses GraphQL API so belongs in data stack

--- amplify/functions/debug-subscription/resource.ts
+++ amplify/functions/debug-subscription/resource.ts
@@ -4,5 +4,6 @@ export const debugSubscription = defineFunction({
   name: 'debugSubscription',
   entry: './handler.ts',
   timeoutSeconds: 30,
-  memoryMB: 512
+  memoryMB: 512,
+  resourceGroupName: 'data' // Assign to data stack since it accesses GraphQL
 });
